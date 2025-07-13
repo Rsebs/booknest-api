f6f47bfe695a551bccc9d87aeb3f3b665ec427f4
Rsebs <sebastianruizj2014@gmail.com>
2025-07-12 22:33:34 -0500

    Se agrega la ruta de login para acceder a rutas protegidas

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\AuthLoginRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use PhpHelpers\Helpers\ApiResponse;
+
+class AuthController extends Controller
+{
+    public function login(AuthLoginRequest $request)
+    {
+        try {
+            if (!Auth::attempt($request->only('email', 'password')))
+                throw new \Exception('Wrong Credentials', 401);
+
+            $user = Auth::user();
+            if (!($user instanceof User))
+                throw new \Exception('Bad request', 422);
+
+            $user->tokens()->delete();
+
+            $token = $user->createToken('api-token', ['*'], now()->addHours(24));
+
+            $loginResponse = [
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+                'avatar' => $user->avatar,
+                'api_token' => $token->plainTextToken,
+            ];
+            return ApiResponse::successResponse($loginResponse, 'Login successful!');
+        } catch (\Throwable $th) {
+            return ApiResponse::errorResponse($th->getMessage(), 'An error occurred', $th->getCode());
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\FeatureController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('user', UserController::class);
+});
 
-Route::apiResource('user', UserController::class);
+Route::post('login', [AuthController::class, 'login'])->name('auth.login');
+
 Route::get('features', [FeatureController::class, 'index'])->name('features.index');
 Route::apiResource('books', BookController::class)->only(['index', 'show']);
