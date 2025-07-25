07ee61121a8cd57eb22929505a0ed46cfcc24285
Rsebs <sebastianruizj2014@gmail.com>
2025-07-25 04:06:56 -0500

    Se elimina el UserController

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,10 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\FeatureController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('user', UserController::class);
+    // Protected routes
 });
 
 Route::post('login', [AuthController::class, 'login'])->name('auth.login');
